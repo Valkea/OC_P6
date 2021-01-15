49ba1c989b09f84a248f1103bf7fc81e4a5e6640
Valkea <github-sub@shedge.com>
2021-01-15 17:23:03 +0100

    feat: Featch and display the categories, the best films, the top film, and the modal content

--- js/script.js
+++ js/script.js
@@ -1,32 +1,134 @@
+var api_url = "http://localhost:8000/api/v1/"
+var modal =  null;
 
-function setModalField(json, field_name, id=null){
+// --- CATEGORIES ---
 
-	try {
-		if( field_name in json )
-		{
-			value = json[field_name]
-			if( id != null) {
-				id = id;	
-			} else {
-				id = field_name;
-			}
+function get_best_cat(num, cat){
+	let results_per_page = 5;
+	let num_pages = Math.ceil(num/results_per_page);
 
-			if( Array.isArray(value) )
-			{
-				value = value.join(", ");
-			}
+	let cat_names = new Map([
+		['cat_0',''],
+		['cat_1','Sci-Fi'],
+		['cat_2','Mystery'],
+		['cat_3','Animation'],
+	]);
 
-			document.getElementById("modal-"+id).innerHTML = value;
+	const urls = [];
+	for(let i = 1; i <= num_pages; i++){
+		urls.push(api_url+"titles/?sort_by=-imdb_score,-votes%2C-votes&genre="+cat_names.get(cat)+"&page="+i);
+	}
 
-		} else {
-			document.getElementById("modal-"+field_name).innerHTML = "NOT FOUND";
+	fetchMulti(urls, save_data, cat, num, fill_category)
+}
+
+const data = {'cat_0':[], 'cat_1':[], 'cat_2':[], 'cat_3':[]}
+
+function save_data(num, cat, fetches, callback){
+	console.log("save_data:", fetches);
+
+	data[cat] = [];
+	let count = 0;
+	for (const fetch of fetches){
+		results = fetch['results']
+		for (const r of results){
+			if(count >= num) break;
+			count ++;
+			data[cat].push(r);
 		}
-	} catch (error) {
-  		console.error(error);
+	}
+
+	console.log("Et la ?", data[cat], data[cat].length);
+	if(cat == 'cat_0'){
+		fill_tops()	
+	} else {
+		fill_category(cat)
 	}
 }
 
-function collect(json){
+function fill_category(cat){
+	
+	let index = 0;
+	for(result of data[cat]){
+		console.log("result:", result);
+		document.getElementById(cat+"_"+index).style.backgroundImage="url("+result['image_url']+")";
+		document.getElementById(cat+"_"+index).innerHTML= result['title'];
+		index ++;
+	}
+}
+
+function fill_tops(){
+	data['first'] = data['cat_0'].shift()
+	fill_category( 'cat_0' )
+
+	first_id = data['first']['id']
+	// document.getElementsByClassName("poster")[0].style.backgroundImage="url("+first['image_url']+")";
+	fetchOne(api_url+"titles/"+first_id, collect_display_first);
+}
+
+// --- FIRST / TOP ---
+
+function collect_display_first(json){
+	console.log(json);
+	
+	document.getElementsByClassName("poster")[0].style.backgroundImage="url("+json['image_url']+")";
+	document.getElementsByClassName("poster__title")[0].innerHTML=json['title'];
+	document.getElementsByClassName("poster__desc")[0].innerHTML=json['description'];
+}
+
+// --- VIGNETTES ---
+
+function open_vignette(ref) {
+	const regExpr = /(cat_[\d+])_([\d+])*/
+	const match = ref.match(regExpr);
+	get_modal_film(data[match[1]][match[2]]['id'])
+}
+
+// --- FETCH & DISPLAY MODAL ---
+
+function fetchOne(url, callback){
+
+	var myHeaders = new Headers();
+
+	var myInit = { method: 'GET',
+	               headers: myHeaders,
+	               // mode: 'cors',
+	               cache: 'default' };
+
+	return fetch( url, myInit)
+	.then( response => response.json() )
+  	.then( json => callback(json) )
+  	.catch( error => console.error('error:', error) );
+}
+
+function fetchMulti(urls, callback, cat, num){
+
+	var myHeaders = new Headers();
+
+	var myInit = { method: 'GET',
+	               headers: myHeaders,
+	               // mode: 'cors',
+	               cache: 'default' };
+
+	fetches = []
+	for (const url of urls){
+		console.log("ADD:", url);
+		fetches.push(fetch(url, myInit))
+	}
+
+	Promise.all(fetches)
+	.then( responses => Promise.all(responses.map(response => response.json())) )
+  	.then( json => callback(num, cat, json) )
+  	.catch( error => console.error('error:', error) );
+}
+
+// --- MODAL ---
+
+function get_modal_film(id){
+	fetchOne(api_url+"titles/"+id, collect_display_modal);
+}
+
+function collect_display_modal(json){
 	console.log(json);
 	
 	document.getElementById("modal-image").src = json['image_url'];
@@ -38,42 +140,51 @@ function collect(json){
 	setModalField(json, 'actors');
 	setModalField(json, 'duration');
 	setModalField(json, 'countries');
-	setModalField(json, 'worldwide_gross_income');
-	setModalField(json, 'description');
+	setModalField(json, 'worldwide_gross_income', null, currencyFormat);
+	setModalField(json, 'long_description');
 	setModalField(json, 'rated');
 	setModalField(json, 'imdb_score');
 	setModalField(json, 'imdb_score', 'imdb_score--mobile');
-}
-
-function callTest(url){
-
-	var myHeaders = new Headers();
-
-	var myInit = { method: 'GET',
-	               headers: myHeaders,
-	               // mode: 'cors',
-	               cache: 'default' };
-
-	fetch( url, myInit)
-	.then( response => response.json() )
-  	.then( json => collect(json) )
-  	.catch( error => console.error('error:', error) );
-}
-
-function currencyFormat(num) {
-  return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-}
-
-function open_modal(modal, ref) {
-	alert("Coucou "+ref);
+	   
 	modal.style.display = "block";
 }
 
+function setModalField(json, field_name, id=null, format=null){
+
+	try {
+		let retv = "NOT FOUND";
+		id = id!=null ? id : field_name;
+		document.getElementById("modal-"+id).innerHTML = "";
+
+		if( field_name in json )
+		{
+			let value = json[field_name] // Get value
+
+			if(Array.isArray(value)) value = value.join(", "); // Array to str
+
+			if(format != null && value != null) value = format(value); // Optional formatting
+
+			retv = value;
+		}
+
+		document.getElementById("modal-"+id).innerHTML = retv;
+
+	} catch (error) {
+  		console.error(error);
+	}
+}
+
+// --- UTILS ---
+
+function currencyFormat(num) {
+	console.log("num:", num);
+	return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
 
 window.onload = function() {
 
 	// --- Get the modal ---
-	let modal = document.getElementById("modal-film");
+	modal = document.getElementById("modal-film");
 	
 	// --- Get the <span> element closeing the modal ---
 	let close_btn = document.getElementsByClassName("modal__close")[0];
@@ -85,28 +196,33 @@ window.onload = function() {
     		if (elems[i].id && elems[i].id.indexOf("cat_") != -1) {
 			// vignettes_btns.push(elems[i]);
 			elems[i].onclick = function(){
-				open_modal(modal, elems[i].id);
+				open_vignette(elems[i].id);
 			}
     		}
 	}
 	// console.log(vignettes_btns);
-
+	
+	// --- Set Best film btn ---
+	document.getElementById("modal__open").onclick = function() {
+		get_modal_film(data['first']['id'])
+	}
 
 	// --- Close the modal when clicking (x) ---
 	close_btn.onclick = function() {
-	  modal.style.display = "none";
+		modal.style.display = "none";
 	}
 
 	// --- Close the modal when the user clicks anywhere outside of it ---
 	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
+		if (event.target == modal) {
+	    		modal.style.display = "none";
+		}
 	}
 
-	//modal.style.display = "block"; // TODO tmp
-	//callTest("http://localhost:8000/api/v1/titles/499549");
-	callTest("http://localhost:8000/api/v1/titles/9");
-	//callTest("http://localhost:8000/api/v1/titles/?format=json");
-	//callTest("http://localhost:8000/api/v1/titles/");
+	// --- Load content ---
+	
+	get_best_cat(8, 'cat_0');
+	get_best_cat(7, 'cat_1');
+	get_best_cat(7, 'cat_2');
+	get_best_cat(7, 'cat_3');
 }
