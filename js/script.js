e9439ccac9c22dec96f005c92870f4f5a16d308e
Valkea <github-sub@shedge.com>
2021-01-15 01:11:21 +0100

    feat: Collect and display modal data from a given movie URI

--- js/script.js
+++ js/script.js
@@ -1,4 +1,71 @@
 
+function setModalField(json, field_name, id=null){
+
+	try {
+		if( field_name in json )
+		{
+			value = json[field_name]
+			if( id != null) {
+				id = id;	
+			} else {
+				id = field_name;
+			}
+
+			if( Array.isArray(value) )
+			{
+				value = value.join(", ");
+			}
+
+			document.getElementById("modal-"+id).innerHTML = value;
+
+		} else {
+			document.getElementById("modal-"+field_name).innerHTML = "NOT FOUND";
+		}
+	} catch (error) {
+  		console.error(error);
+	}
+}
+
+function collect(json){
+	console.log(json);
+	
+	document.getElementById("modal-image").src = json['image_url'];
+
+	setModalField(json, 'title');
+	setModalField(json, 'genres');
+	setModalField(json, 'date_published');
+	setModalField(json, 'directors');
+	setModalField(json, 'actors');
+	setModalField(json, 'duration');
+	setModalField(json, 'countries');
+	setModalField(json, 'worldwide_gross_income');
+	setModalField(json, 'description');
+	setModalField(json, 'rated');
+	setModalField(json, 'imdb_score');
+	setModalField(json, 'imdb_score', 'imdb_score--mobile');
+}
+
+function callTest(url){
+
+	var myHeaders = new Headers();
+
+	var myInit = { method: 'GET',
+	               headers: myHeaders,
+	               // mode: 'cors',
+	               cache: 'default' };
+
+	fetch( url, myInit)
+	.then( response => response.json() )
+  	.then( json => collect(json) )
+  	.catch( error => console.error('error:', error) );
+}
+
+function currencyFormat(num) {
+  return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+
+
 window.onload = function() {
 
 	// Get the modal
@@ -28,6 +95,10 @@ window.onload = function() {
 	}
 
 	modal.style.display = "block"; // TODO tmp
+	//callTest("http://localhost:8000/api/v1/titles/499549");
+	callTest("http://localhost:8000/api/v1/titles/9");
+	//callTest("http://localhost:8000/api/v1/titles/?format=json");
+	//callTest("http://localhost:8000/api/v1/titles/");
 }
 
 // --- INFOS ---
