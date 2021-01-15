aa00c73664cfb3063feb5d71542534d668717f45
Valkea <github-sub@shedge.com>
2021-01-15 01:36:15 +0100

    feat: Add onclick actions to the vignettes, and collect their respective IDs

--- js/script.js
+++ js/script.js
@@ -64,55 +64,49 @@ function currencyFormat(num) {
   return '$' + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
+function open_modal(modal, ref) {
+	alert("Coucou "+ref);
+	modal.style.display = "block";
+}
 
 
 window.onload = function() {
 
-	// Get the modal
+	// --- Get the modal ---
 	let modal = document.getElementById("modal-film");
 	
-	// Get the button that opens the modal
-	let open_btn = document.getElementById("modal__open");
-	
-	// Get the <span> element that closes the modal
+	// --- Get the <span> element closeing the modal ---
 	let close_btn = document.getElementsByClassName("modal__close")[0];
 
-	// When the user clicks on the button, open the modal
-	open_btn.onclick = function() {
-	  modal.style.display = "block";
+	// --- Set onclick actions to open the modals ---
+	let elems = document.getElementsByTagName("*");
+	// let vignettes_btns = [];
+	for (let i=0, m=elems.length; i<m; i++) {
+    		if (elems[i].id && elems[i].id.indexOf("cat_") != -1) {
+			// vignettes_btns.push(elems[i]);
+			elems[i].onclick = function(){
+				open_modal(modal, elems[i].id);
+			}
+    		}
 	}
-	
-	// When the user clicks on <span> (x), close the modal
+	// console.log(vignettes_btns);
+
+
+	// --- Close the modal when clicking (x) ---
 	close_btn.onclick = function() {
 	  modal.style.display = "none";
 	}
 
-	// When the user clicks anywhere outside of the modal, close it
+	// --- Close the modal when the user clicks anywhere outside of it ---
 	window.onclick = function(event) {
 	  if (event.target == modal) {
 	    modal.style.display = "none";
 	  }
 	}
 
-	modal.style.display = "block"; // TODO tmp
+	//modal.style.display = "block"; // TODO tmp
 	//callTest("http://localhost:8000/api/v1/titles/499549");
 	callTest("http://localhost:8000/api/v1/titles/9");
 	//callTest("http://localhost:8000/api/v1/titles/?format=json");
 	//callTest("http://localhost:8000/api/v1/titles/");
 }
-
-// --- INFOS ---
-//
-// document.body.style.backgroundImage = "url('img_tree.png')";
-// document.getElementById("myImg").src = "hackanm.gif";
-// modal-title
-// modal-genre
-// modal-date
-// modal-rated
-// modal-imdb-score
-// modal-director
-// modal-actors
-// modal-duration
-// modal-country
-// modal-results
-// modal-summary
